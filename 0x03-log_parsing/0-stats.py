a80530aef2e2754a34f035d5630e86e0c44de386
OmobaVII <omoba.career@gmail.com>
2023-10-19 17:25:00 +0000

    Implement logparsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -13,6 +13,7 @@ def print_stats(dict_code_stat, file_size):
         if v != 0:
             print("{}: {}".format(k, v))
 
+
 file_size = 0
 code_stat = 0
 count = 0
@@ -27,15 +28,15 @@ dict_code_stat = {"200": 0,
 
 try:
     for line in stdin:
-        entered_line = line.split() # splits read line
-        entered_line = entered_line[::-1] # inverts the splitted line
+        entered_line = line.split()  # splits read line
+        entered_line = entered_line[::-1]  # inverts the splitted line
 
         if len(entered_line) > 2:
             count += 1
 
             if count <= 10:
-                file_size += int(entered_line[0]) # the file size
-                code_stat = entered_line[1] # status code
+                file_size += int(entered_line[0])  # the file size
+                code_stat = entered_line[1]  # status code
 
                 if (code_stat in dict_code_stat.keys()):
                     dict_code_stat[code_stat] += 1
