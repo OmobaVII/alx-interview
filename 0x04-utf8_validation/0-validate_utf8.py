2cc1159134ce02e5d99bd3a6dcf7f63ee4ce9b65
OmobaVII <omoba.career@gmail.com>
2023-10-27 10:55:20 +0000

    Correction

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -26,8 +26,8 @@ def validUTF8(data):
                 number_bytes = 2
             elif (byte >> 3) == 0b11110:
                 number_bytes = 3
-            else:
-                return False
+            elif (byte >> 6) != 0b10:
+                number_bytes += 1
 
         else:
             # checks if it is a continuation byte
